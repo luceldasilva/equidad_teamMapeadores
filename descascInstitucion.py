5469f6f73bde567aa673cc6c5332110fe916ea98
addticsoporte <addticsoporte@gmail.com>
2022-12-02 17:25:59 -0600

    Este se guarda aqui en caso de emergencia de querer volver a leer el retorno de la lista de ramas dependencias, se va a probar el de ramaTesting

---
+++ descascInstitucion.py
@@ -0,0 +1,18 @@
+import json
+archivo = open('./s2/SINALOA/data-0000000001.json',encoding="utf8") # 1 . Se ubica el archivo JSON para abrirlo. 
+
+datos = json.load(archivo) #2. Se carga el archivo JSON dentro del ecosistema de analisis de JSON
+
+counter = 0
+
+def retrieve(x):
+    try:
+        return x['ramo']['valor']
+    except (KeyError, TypeError):
+        return False
+for x in datos:
+    valor = retrieve(x)
+    print(valor)
+    counter += 1
+print(counter) 
+#Esto es para guardarlo de emergencia
